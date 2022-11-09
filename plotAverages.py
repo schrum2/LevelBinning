a077e84991ca9096b0d86cf7171731170a769dc4
Jacob Schrum <schrum2@southwestern.edu>
2022-11-09 13:53:29 -0600

    Update plotAverages.py
    @amykhoover
    Plot the average across all files of a given set of experiments

--- plotAverages.py
+++ plotAverages.py
@@ -4,85 +4,117 @@ import pandas
 from functools import reduce
 import matplotlib.pyplot as plt
 
-# python plotAverages.py <experiment dir> <log prefix> <log suffix>
+def plot_column(avg_data, col):
+    global titles
+    # Restricted counts
+    df = pandas.DataFrame({
+    'FullDirect2GAN': avg_data["FullDirect2GAN"][col],
+    'RestrictedDirect2GAN': avg_data["RestrictedDirect2GAN"][col]
+    }, index=avg_data["FullDirect2GAN"][0])
+    df.plot.line()
+    plt.title(titles[col])
+    plt.show()
 
-n = len(sys.argv)
-if n != 4:
-    print("Specify a root experiment directory and log file prefix and suffix, as in: ")
-    print("python plotAverages.py mariopdsl MarioPDSL- _Fill_log.txt")
-    exit(1)
+if __name__ == "__main__":
 
-directory = sys.argv[1]
-log_prefix = sys.argv[2]
-log_suffix = sys.argv[3]
-entries = os.listdir(directory)
-# For example: FullDirect2GAN0, FullDirect2GAN1, ..., FullDirect2GAN9, RestrictedDirect2GAN0, RestrictedDirect2GAN1, ..., RestrictedDirect2GAN9
+    # python plotAverages.py <experiment dir> <log prefix> <log suffix>
 
-all_data = dict()
-prefixes = set()
+    n = len(sys.argv)
+    if n != 4:
+        print("Specify a root experiment directory and log file prefix and suffix, as in: ")
+        print("python plotAverages.py mariopdsl MarioPDSL- _Fill_log.txt")
+        exit(1)
 
-min_num_rows = float("inf")
+    global titles
+    titles = dict()
+    titles[1] = 'Filled Bins Across Whole Archive'
+    titles[2] = 'QD Across Whole Archive'
+    titles[3] = 'Maximum Fitness Score Across Whole Archive'
+    titles[4] = 'Number of Discarded Individuals'
+    titles[5] = 'Filled Bins in the Restricted Region'
+    titles[6] = 'QD in the Restricted Region'
+    titles[7] = 'Maximum Fitness Score in the Restricted Region'
 
-for dir in entries:
-    name_len = len(dir)
-    index = name_len - 1
-    while dir[index].isnumeric():
-        index -= 1
 
-    # Index is last index before the numeric portion
-    prefix = dir[0:(index+1)]
-    prefixes.add(prefix)
-    experiment_num = int(dir[(index+1):name_len])
-    if not prefix in all_data:
-        all_data[prefix] = dict()
+    directory = sys.argv[1]
+    log_prefix = sys.argv[2]
+    log_suffix = sys.argv[3]
+    entries = os.listdir(directory)
+    # For example: FullDirect2GAN0, FullDirect2GAN1, ..., FullDirect2GAN9, RestrictedDirect2GAN0, RestrictedDirect2GAN1, ..., RestrictedDirect2GAN9
 
-    #print(prefix, experiment_num)
-    #input()
+    all_data = dict()
+    prefixes = set()
 
-    complete_file_path = f"{directory}/{dir}/{log_prefix}{dir}{log_suffix}"
+    min_num_rows = float("inf")
 
-    all_data[prefix][experiment_num] = pandas.read_csv(complete_file_path, header=None, sep='\t', engine='python') # lineterminator='\r')
-    num_rows = all_data[prefix][experiment_num].shape[0]
-    min_num_rows = min(min_num_rows,num_rows)
+    for dir in entries:
+        name_len = len(dir)
+        index = name_len - 1
+        while dir[index].isnumeric():
+            index -= 1
 
-    #print(all_data[prefix][experiment_num])
+        # Index is last index before the numeric portion
+        prefix = dir[0:(index+1)]
+        prefixes.add(prefix)
+        experiment_num = int(dir[(index+1):name_len])
+        if not prefix in all_data:
+            all_data[prefix] = dict()
 
-    #with open(complete_file_path) as log:
-    #    for line in log:
-    #        values = map(float,line.split())
-    #        col = 0
-    #        all_data[prefix][experiment_num]["col"+col] = list()
-    #        for v in values:
-
-#print(min_num_rows)  
-avg_data = dict()
-
-for p in prefixes:
-    frame_list = []
-    for num in all_data[p]:
-        if all_data[p][num].shape[0] > min_num_rows:
-            # drop a row since it is one longer
-            all_data[p][num].drop(all_data[p][num].tail(1).index,inplace=True)
-        
-        #print(all_data[p][num])
+        #print(prefix, experiment_num)
         #input()
 
-        frame_list.append(all_data[p][num])
+        complete_file_path = f"{directory}/{dir}/{log_prefix}{dir}{log_suffix}"
 
-    #print(frame_list)
-    #input()
-    sum_frame = reduce(lambda a, b: a.add(b, fill_value=0), frame_list)
-    #print(sum_frame)
-    #input()
-    avg_frame = sum_frame.div(len(all_data[p]))
-    #print(avg_frame)
-    #input()
-    avg_data[p] = avg_frame
+        all_data[prefix][experiment_num] = pandas.read_csv(complete_file_path, header=None, sep='\t', engine='python') # lineterminator='\r')
+        num_rows = all_data[prefix][experiment_num].shape[0]
+        min_num_rows = min(min_num_rows,num_rows)
 
-    # TODO: Plot all lines together with column 0 as the x-axis, and each other one on y-axis
-    print("Plot")
-    avg_frame.plot.line(0,1)
-    plt.show()
-    avg_frame.plot.line(0,2)
-    plt.show()
-    input()
+        #print(all_data[prefix][experiment_num])
+
+        #with open(complete_file_path) as log:
+        #    for line in log:
+        #        values = map(float,line.split())
+        #        col = 0
+        #        all_data[prefix][experiment_num]["col"+col] = list()
+        #        for v in values:
+
+    #print(min_num_rows)  
+    avg_data = dict()
+
+    for p in prefixes:
+        frame_list = []
+        for num in all_data[p]:
+            if all_data[p][num].shape[0] > min_num_rows:
+                # drop a row since it is one longer
+                all_data[p][num].drop(all_data[p][num].tail(1).index,inplace=True)
+            
+            #print(all_data[p][num])
+            #input()
+
+            frame_list.append(all_data[p][num])
+
+        #print(frame_list)
+        #input()
+        sum_frame = reduce(lambda a, b: a.add(b, fill_value=0), frame_list)
+        #print(sum_frame)
+        #input()
+        avg_frame = sum_frame.div(len(all_data[p]))
+        #print(avg_frame)
+        #input()
+        avg_data[p] = avg_frame
+
+
+
+    # Restricted counts
+    #df = pandas.DataFrame({
+    #'FullDirect2GAN-Full': avg_data["FullDirect2GAN"][3],
+    #'FullDirect2GAN-Restricted': avg_data["FullDirect2GAN"][7],
+    #'RestrictedDirect2GAN': avg_data["RestrictedDirect2GAN"][3]
+    #}, index=avg_data["FullDirect2GAN"][0])
+    #df.plot.line()
+    #plt.show()
+
+    # Plot each possible average
+    for i in range(1,8):
+        plot_column(avg_data, i) 
+    
